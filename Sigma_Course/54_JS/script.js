6e2d4facdeed0f886ea56e8e45d494bee3f2bc3d
C0DESbyKeshav <keshavmandal2503@gmail.com>
2025-07-21 20:32:42 +0530

    excited for JS

---
+++ Sigma_Course/54_JS/script.js
@@ -0,0 +1,29 @@
+alert("You are getting too comfortable bro");
+
+console.log("Stay Strong bro !!");
+console.log("Never ever give up....");
+console.log("Consistency is the key");
+
+var name = prompt("Enter your name:");
+console.log("Thankyou " + name + " for not skipping even a day.");
+
+var doOrDie = confirm("Are you sure you want to quit and keep living the mediocre life ?");
+if (doOrDie) {
+    console.log("Welcome to the Rat Race bro.");
+} else {
+    console.log("Welcome to the Grind Club bro.");
+    console.log("Feel the process buddy.");
+}
+
+document.title = "JavaScript"
+// Changes the title of the webpage.
+
+document.body.style.backgroundColor = "yellow"
+// Adds inline css.
+
+// JavaScript and Node.js is completely same language in terms of all the syntax.
+// JavaScript => Runs the .js file with integration to index.html in the browser.
+// Node.js => Runs the .js file in terminal.
+
+// Just like we use 'pip' to install any package in python.
+// Same way there's 'npm' in node.js
