cbbb4f3571e2a639fc7bd48fbce729ca418d19d7
C0DESbyKeshav <keshavmandal2503@gmail.com>
2025-08-07 07:00:29 +0530

    JS browser

--- Sigma_Course/65_Ex11-Factorial/script.js
+++ Sigma_Course/65_Ex11-Factorial/script.js
@@ -3,7 +3,34 @@ let num = prompt("Enter a non-negative integer:");
 // * M-1: Using for loop
 let fact = num;
 
-for (let i = 1; i < num; i++) {
-  fact *= i;
+if (num == 0) {
+  alert(`The factorial of ${num} is 1 by definition.`);
+} else if (num > 0) {
+  for (let i = 1; i < num; i++) {
+    fact *= i;
+  }
+  alert(`The factorial of ${num} using loop is ${fact}`);
+} else {
+  alert("Please enter a non-negative integer.");
 }
-alert(`The factorial of ${num} is ${fact}`);
+
+// * M-II: Using reduce
+let arr = [];
+
+if (num == 0) {
+  alert(`The factorial of ${num} is 1 by definition.`);
+} else if (num > 0) {
+  for (let i = 0; i < num; i++) {
+    arr[i] = i + 1;
+  }
+
+  const factorial = (a, b) => {
+    return a * b;
+  };
+
+  let ans = arr.reduce(factorial);
+
+  alert(`The factorial of ${num} using reduce function is ${ans}`);
+} else {
+  alert("Please enter a non-negative integer.");
+}
